61194c1ff1b0d1313ca5a712ad44dcc5f32f48cc
Jose Antonio Esquivel <jesquivel@aeropost.com>
2016-09-21 09:14:55 -0600

    v2

---
+++ include/types.h
@@ -0,0 +1,5 @@
+
+struct button_address {
+	uint16_t	button_panel_addr;
+	uint8_t		button;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,7 @@
 #include <Souliss.h>
 
 #include "../../ControllerGateway/include/constants.h"
+#include "../../ControllerGateway/include/types.h"
 
 void setup()
 {
@@ -30,11 +31,12 @@ void setup()
 	// Set network parameters
 	SetAddress(RS485_GATEWAY_ADDR, VNET_SUBNET_MASK, RS485_GATEWAY_ADDR);
 
+	vNet_SetMulticastGroup(BUTTONS_PANEL_MCAS_ADDR, 0);
+
 	Set_T11(GATEWAY_LIGHT_1_SLOT);
 	Set_T11(GATEWAY_LIGHT_2_SLOT);
 
-	Set_T11(GATEWAY_BUTTONS_PANEL_1_BUTTON_1_SLOT);
-	Set_T11(GATEWAY_BUTTONS_PANEL_1_BUTTON_1_SLOT);
+	pinMode(13, OUTPUT); // Debug
 
 }
 
@@ -48,35 +50,46 @@ void loop()
 		// cames directly from SoulissApp or the push-button located on the other node
 		FAST_50ms() {
 
-			// Copy input from BUTTONS to LIGHTS
-			mInput(GATEWAY_LIGHT_1_SLOT) = mInput(GATEWAY_BUTTONS_PANEL_1_BUTTON_1_SLOT);
-			//
-			Logic_T11(GATEWAY_BUTTONS_PANEL_1_BUTTON_1_SLOT);
-			Logic_T11(GATEWAY_BUTTONS_PANEL_1_BUTTON_2_SLOT);
-
-			//
-			//void Souliss_LinkOI(U8 *memory_map, U8 input_slot, U8 output_slot)
-			//{
-			//	if(memory_map[MaCaco_OUT_s + output_slot] != 0x00)
-			//		memory_map[MaCaco_IN_s + input_slot] = memory_map[MaCaco_OUT_s + output_slot];	// Link IO
-			//}
-			//Souliss_LinkOI(memory_map, GATEWAY_LIGHT_1_SLOT, GATEWAY_BUTTONS_PANEL_1_BUTTON_1_SLOT);
-			//sSouliss_LinkOI(memory_map, GATEWAY_LIGHT_2_SLOT, GATEWAY_BUTTONS_PANEL_1_BUTTON_2_SLOT);
-			// Link GATEWAY_BUTTONS_PANEL_1_BUTTON_1_SLOT To GATEWAY_LIGHT_1_SLOT
-			// Link GATEWAY_BUTTONS_PANEL_1_BUTTON_2_SLOT To GATEWAY_LIGHT_2_SLOT
-			//
-
-			if (Logic_T11(GATEWAY_LIGHT_1_SLOT)) {
-				Send(RS485_BUTTONS_PANEL_1_ADDR, BUTTONS_PANEL_1_LED_1_SLOT, mOutput(GATEWAY_LIGHT_1_SLOT));
-				Send(RS485_RELAYS_1_ADDR, RELAYS_1_RELAY_1_SLOT, mOutput(GATEWAY_LIGHT_1_SLOT));
-			}
-			if (Logic_T11(GATEWAY_LIGHT_2_SLOT)) {
-				Send(RS485_BUTTONS_PANEL_1_ADDR, BUTTONS_PANEL_1_LED_2_SLOT, mOutput(GATEWAY_LIGHT_2_SLOT));
-				Send(RS485_RELAYS_1_ADDR, RELAYS_1_RELAY_2_SLOT, mOutput(GATEWAY_LIGHT_2_SLOT));
+			boolean button_pressed = false;
+			button_address button;
+			uint8_t len;
+			if (subscribedata(BUTTON_PUSHED_EVENT, (uint8_t*)&button, &len)) {
+				if (button.button_panel_addr == RS485_BUTTONS_PANEL_1_ADDR && button.button == 1) {
+					mInput(GATEWAY_LIGHT_1_SLOT) = Souliss_T1n_ToggleCmd;
+					button_pressed = true;
+				}
+				if (button.button_panel_addr == RS485_BUTTONS_PANEL_1_ADDR && button.button == 2) {
+					mInput(GATEWAY_LIGHT_2_SLOT) = Souliss_T1n_ToggleCmd;
+					button_pressed = true;
+				}
 			}
 
-			//DigOut(LED_1_PIN, Souliss_T1n_Coil, GATEWAY_BUTTONS_PANEL_1_BUTTON_1_SLOT);
+			Logic_T11(GATEWAY_LIGHT_1_SLOT);
+			Logic_T11(GATEWAY_LIGHT_2_SLOT);
 
+			if (button_pressed && button.button_panel_addr == RS485_BUTTONS_PANEL_1_ADDR && button.button == 1) {
+				uint8_t cmd;
+	            if(mOutput(GATEWAY_LIGHT_1_SLOT))
+	                cmd = Souliss_T1n_OnCmd;
+	            else
+	                cmd = Souliss_T1n_OffCmd;
+				Send(RS485_RELAYS_1_ADDR, RELAYS_MODULE_RELAY_1_SLOT, cmd);
+				Send(RS485_BUTTONS_PANEL_1_ADDR, BUTTONS_PANEL_LED_1_SLOT, cmd);
+				//digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+			}
+			if (button_pressed && button.button_panel_addr == RS485_BUTTONS_PANEL_1_ADDR && button.button == 2) {
+				uint8_t cmd;
+	            if(mOutput(GATEWAY_LIGHT_2_SLOT))
+	                cmd = Souliss_T1n_OnCmd;
+	            else
+	                cmd = Souliss_T1n_OffCmd;
+				Send(RS485_BUTTONS_PANEL_1_ADDR, BUTTONS_PANEL_LED_2_SLOT, cmd);
+				Send(RS485_RELAYS_1_ADDR, RELAYS_MODULE_RELAY_2_SLOT, cmd);
+				//digitalWrite(13, LOW);   // turn the LED on (HIGH is the voltage level)
+			}
+
+			// Check GATEWAY_LIGHT_1_SLOT status
+			DigOut(13, Souliss_T1n_Coil, GATEWAY_LIGHT_1_SLOT);
 		}
 
 		// Process the communication, this include the command that are coming from SoulissApp
